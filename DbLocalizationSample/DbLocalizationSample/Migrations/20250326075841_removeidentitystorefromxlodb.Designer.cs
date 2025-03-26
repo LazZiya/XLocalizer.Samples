9faabfe89eee560c8b47135d3cc16e21500d8640
Ziya Mollamahmut <ziyadmm@gmail.com>
2025-03-26 10:59:11 +0300

    remove identity from XLocalizerDBContext

--- DbLocalizationSample/DbLocalizationSample/Migrations/20250326075841_removeidentitystorefromxlodb.Designer.cs
+++ DbLocalizationSample/DbLocalizationSample/Migrations/20250326075841_removeidentitystorefromxlodb.Designer.cs
@@ -3,6 +3,7 @@ using DBLocalizationSample.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DbLocalizationSample.Migrations
 {
     [DbContext(typeof(XLocalizerDbContext))]
-    partial class XLocalizerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250326075841_removeidentitystorefromxlodb")]
+    partial class removeidentitystorefromxlodb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
